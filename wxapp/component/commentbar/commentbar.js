5f3d4c65725a1eb1e14ff9874bc2ea8c3993b5b6
史文焕 <1239417773@qq.com>
2018-07-17 18:33:49 +0800

    新的一天

--- wxapp/component/commentbar/commentbar.js
+++ wxapp/component/commentbar/commentbar.js
@@ -4,19 +4,38 @@ Component({
    */
   properties: {
     comments: Array
+
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    writeReview: '',
+    show_commentView: false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    StartedReview() {
+      let self = this;
+      self.setData({
+        show_commentView: true
+      })
+    },
+    unselect(res) {
+      let self = this;
+      self.setData({
+        show_commentView: false
+      })
+    },
+    IdentifyingChoice(res) {
+      let self = this;
+      self.setData({
+        show_commentView: false
+      })
+    }
   }
 })
